62e8aedc65a82a01de48e4e8c1e1d6eb5c93482d
Alexander <alexanderkoch@Alexanderkochs-MacBook-Air.local>
2015-10-14 23:01:39 +0200

    Added shop
    Added a basic shop with a item

--- Game.js
+++ Game.js
@@ -9,12 +9,26 @@ var sellButton;
 var bg;
 var hotDogButton;
 var hotdogs;
+var shopButtton;
+var shopOpen = false;
+var shopFrame;
+
+//HotDog storage vars
+var hotDogStorageBuyButton;
+var hotDogStorageText;
+var hotDogStoragePrice = 10;
+var hotDogStoragePriceText;
+var hotDogStorageAmount = 0; 
+var hotDogStorageAmountText;
 
 function preload(){
     game.load.image('hotdog', 'hotDog.png');
     game.load.image("bg", "bg.png");
     game.load.image("sellButton", "sellAll.png");
     game.load.image("hotdogBut", "hotdogButton.png");
+    game.load.image("shopBut", "shopButton.png");
+    game.load.image("shopFrame", "shopFrame.png");
+    game.load.image("testassets1", "star.png");
 }
 
 function create(){
@@ -33,6 +47,10 @@ function create(){
     hotDogButton.events.onInputDown.add(addHotDog, this);
     hotDogButton.inputEnabled = true;
     
+    shopButtton = game.add.sprite(700 - 110 - 20, 700 - 150, "shopBut");
+    shopButtton.events.onInputDown.add(openShop, this);
+    shopButtton.inputEnabled = true;
+    
 }
 
 function addHotDog(){
@@ -40,13 +58,79 @@ function addHotDog(){
         hotDog = game.add.sprite(Math.floor((Math.random() * 800) + 1), Math.floor((Math.random() * 600) + 1), 'hotdog');
         hotDog.scale.set(0.5);
         hotdogsMade++;
+        
+        //Update the shop on click
+        updateShop();
     }
+}
+
+function openShop(){
+    if(!shopOpen){
+        shopOpen = true;
+        drawShop();
+    }else if(shopOpen){
+        closeShop();
+    }
+}
+
+function drawShop(){
+    //The frame
+    shopFrame = game.add.sprite(700 - 200 - 20, 600 - 300 - 40 - 30, "shopFrame");
+    
+    //Test shopItem
+    hotDogStorageBuyButton = game.add.sprite(700 - 200 + 65, 250 + 22 + 20, "testassets1");
+    //The text
+    hotDogStorageText = game.add.text(495, 250, "Upgrade hotdog storage", { font: "15px Arial",  fill: '#ffffff' });
+    // the buttons input
+    hotDogStorageBuyButton.events.onInputDown.add(buyHotdogStorage, this);
+    hotDogStorageBuyButton.inputEnabled = true;
+    
+    //Price Text
+    hotDogStoragePriceText = game.add.text(495, 300, "Price: "+hotDogStoragePrice, { font: "15px Arial",  fill: '#ffffff' });
+    
+    //How many upgrades you have
+    hotDogStorageAmountText = game.add.text(495, 270, "You have: "+hotDogStorageAmount, { font: "15px Arial",  fill: '#ffffff' });
+
+}
+
+function updateShop(){
+    shopFrame.bringToTop();
+    hotDogStorageText.bringToTop();
+    hotDogStorageAmountText.bringToTop();
+    hotDogStorageBuyButton.bringToTop();
+    hotDogStoragePriceText.bringToTop();
+}
+
+//TODO: Add en utills til at gøre det
+
+function buyHotdogStorage(){
+    if(moneys >= hotDogStoragePrice){
+        hotDogStorageAmount++;
+        moneys -= hotDogStoragePrice;
+        maxHotDogs += 5;
+        hotDogStoragePrice += 20;
+    }
+    
+    //Update the amount text
+    hotDogStorageAmountText.text = "You have: "+hotDogStorageAmount;
+    hotDogStoragePriceText.text = "Price: "+hotDogStoragePrice;
+
+}
+
+function closeShop(){
+    shopOpen = false;
+    shopFrame.kill();
+    
+    hotDogStorageText.kill();
+    hotDogStorageBuyButton.kill();
+    hotDogStorageAmountText.kill();
+    hotDogStoragePriceText.kill();
 }
 
 function sellHotDogs(){
     moneys += hotdogsMade;
     hotdogsMade -= hotdogsMade;
-    console.log("FISK");
+    //console.log("FISK");
 }
 
 function updateText(){
@@ -54,6 +138,7 @@ function updateText(){
     score.bringToTop();
     moneyText.text = ""+moneys+"$";
     moneyText.bringToTop();
+    shopButtton.bringToTop();
     hotDogButton.bringToTop();
     sellButton.bringToTop();
 }
